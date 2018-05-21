b989b8bf0f2e71cd0762a17a1df273e5c92e3815
root <root@vmi181448.contaboserver.net>
2018-05-21 13:02:12 +0200

    Comic initial / Comit inicial

---
+++ ckanext/fundao_theme/plugin.py
@@ -0,0 +1,35 @@
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+
+def most_popular_groups():
+    '''Return a sorted list of the groups with the most datasets.'''
+
+    # Get a list of all the site's groups from CKAN, sorted by number of
+    # datasets.
+    groups = toolkit.get_action('group_list')(
+        data_dict={'sort': 'package_count desc', 'all_fields': True})
+
+    # Truncate the list to the 10 most popular groups only.
+    groups = groups[:10]
+
+    return groups
+
+class Fundao_ThemePlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.ITemplateHelpers)
+    # IConfigurer
+
+    def update_config(self, config_):
+        toolkit.add_template_directory(config_, 'templates')
+        toolkit.add_public_directory(config_, 'public')
+        toolkit.add_resource('fanstatic', 'fundao_theme')
+
+    def get_helpers(self):
+        '''Register the most_popular_groups() function above as a template
+        helper function.
+
+        '''
+        # Template helper function names should begin with the name of the
+        # extension they belong to, to avoid clashing with functions from
+        # other extensions.
+        return {'example_theme_most_popular_groups': most_popular_groups}
